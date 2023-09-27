d1cb8c1b8ad2a0a76034c934cedba20dbfe25412
Kevin Deyne <kevindeyne@gmail.com>
2023-09-26 20:24:40 -0700

    Add files via upload

--- camera.js
+++ camera.js
@@ -1,7 +1,23 @@
+const scanner = new jscanify();
+
 document
   .getElementById("cameraFileInput")
   .addEventListener("change", function () {
+    let imageSrc = window.URL.createObjectURL(this.files[0]);
     document
       .getElementById("pictureFromCamera")
-      .setAttribute("src", window.URL.createObjectURL(this.files[0]));
+      .setAttribute("src", imageSrc);
+
+      const newImg = document.createElement("img")
+      newImg.src = imageSrc
+
+      newImg.onload = function(){
+          const paperWidth = newImg.naturalWidth;
+          const paperHeight = newImg.naturalHeight;
+
+          const resultCanvas = scanner.extractPaper(newImg, paperWidth, paperHeight);
+          document.getElementById("scannedPictureExtract").setAttribute("src", resultCanvas.toDataURL("image/jpeg", 1.0));
+
+      }
+
   });
